876e3deda96a9b9747289bee82e799ba41d8e33b
Gilles Mazet-Roux <gilles.mazet-roux@cea.fr>
2021-05-14 15:47:17 +0000

    changed ppsd_length to 600 sec

--- ppsd_and_npz.py
+++ ppsd_and_npz.py
@@ -109,7 +109,7 @@ for day in pbar:
         trace=st[0]
         print ("ppsd...")
         ppsd = PPSD(trace.stats, inv,
-                ppsd_length=120, overlap=0.5,
+                ppsd_length=600, overlap=0.5,
                 period_smoothing_width_octaves=0.025,
                 period_step_octaves=0.0125,
                 period_limits=(freqmin, freqmax),
